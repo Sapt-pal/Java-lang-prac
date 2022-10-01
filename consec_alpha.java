23a8ef487664ec67f31469ec34c7b7002d101643
Saptarshi Pal <85279914+Sapt-pal@users.noreply.github.com>
2022-10-01 12:55:09 +0530

    Add files via upload
    added consec_alpha.java

---
+++ consec_alpha.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+/**
+ * Print words from a user-input string where adjacent letters are also consecutive alphabetically. Print the frequency of such words.
+ *
+ * @author (Sapt-pal)
+ * @version (1.0.00)
+ */
+public class consec_alpha
+{
+    public static void main()
+    {
+        int i, j, L, c, fn = 0;
+        String st, w = "";
+        char ch, che;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter string: ");
+        st = in.nextLine();
+        L = st.length();
+        for (i = L;i < L;i++){
+            ch = st.charAt(i);c = 0;w  = "";
+            if (ch != ' ')
+            {
+                w += ch;
+                c++;
+            }
+            else
+            {
+                for (j = 0;j < c-1;j++)
+                {
+                    if ((((int) w.charAt(j)) + 1) == (int) w.charAt(j + 1))
+                    {
+                        System.out.println(w);
+                        fn ++;
+                    }
+                }
+                
+            }
+        }
+        System.out.println("Frequency: " + fn);
+    }
+}
